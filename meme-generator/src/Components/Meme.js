0ec5f3cbfc2a9285bff877aa1d1c1d86ba469de6
Kurt Guardia <kurtguardia@gmail.com>
2022-03-02 12:42:21 +0100

    Rendering img

--- meme-generator/src/Components/Meme.js
+++ meme-generator/src/Components/Meme.js
@@ -1,18 +1,42 @@
+import { useState } from 'react';
+import memesData from '../memesData';
+
+const { memes } = memesData.data;
+
 export default function Meme() {
+  const [meme, setMeme] = useState({
+    topText: '',
+    bottomText: '',
+    randomImage: 'https://i.imgflip.com/1bij.jpg',
+  });
+
+  const [allMemeImages] = useState(memes);
+
+  function handleClick() {
+    const n = Math.floor(Math.random() * 100);
+
+    setMeme((prevMeme) => {
+      return { ...prevMeme, randomImage: allMemeImages[n].url };
+    });
+  }
+
   return (
-    <form>
-      <div className='input'>
-        <input type='text' placeholder='Top text' name='top' id='top-text' />
-        <input
-          type='text'
-          placeholder='Bottom text'
-          name='bottom'
-          id='bottom-text'
-        />
+    <>
+      <div className='form'>
+        <div className='input'>
+          <input type='text' placeholder='Top text' name='top' id='top-text' />
+          <input
+            type='text'
+            placeholder='Bottom text'
+            name='bottom'
+            id='bottom-text'
+          />
+        </div>
+        <button onClick={handleClick} className='purple btn' type='submit'>
+          Get a new meme image 🖼️
+        </button>
       </div>
-      <button className='purple btn' type='submit'>
-        Get a new meme image 🖼️
-      </button>
-    </form>
+      <img className='memeImg' src={meme.randomImage} alt='meme' />
+    </>
   );
 }
